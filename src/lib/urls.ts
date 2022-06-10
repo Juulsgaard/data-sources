1137a02c8e5cc477aac3e91f6c9244352cbdafea
Frederik Juulsgaard Sørensen <frederikjs1@hotmail.com>
2022-06-11 00:23:00 +0200

    v1.0.1

--- src/lib/urls.ts
+++
@@ -1,7 +0,0 @@
-
-export function applyQueryParam(url: string, prop: string, value: string) {
-  if (!url) return url;
-  return url.includes('?')
-    ? `${url}&${prop}=${value}`
-    : `${url}?${prop}=${value}`;
-}
