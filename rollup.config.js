7cc6ca97c5b3db564fa8ccdba7225bfa559de81c
Frederik Sørensen <frederikjs1@hotmail.com>
2024-04-08 10:00:22 +0200

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,8 @@ export default [
 			dir: 'dist',
 			format: "esm",
 			preserveModules: true,
-		}
+		},
+		external: ['@juulsgaard/ts-tools', 'rxjs', 'rxjs/operators', '@juulsgaard/rxjs-tools', 'fuse.js']
 	},
 	{
 		input: 'out/index.d.ts',
