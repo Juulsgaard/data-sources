23f009d15b01d76a23ca2209cf41a7df53663765
Frederik Sørensen <frederikjs1@hotmail.com>
2024-04-08 13:13:44 +0200

    Switch to ESBuild

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-export {
+export type {
   TreeFolderData, TreeItemData, TreeRowData, TreeFolder, TreeItem, TreeSearchRowData, TreeItemSearchRowData, TreeFolderSearchRowData,
   TreeSearchColumnConfig, TreeItemMeta, TreeFolderMeta
 } from "./tree-source/tree-data";
 export {TreeDataSource} from "./tree-source/tree-data-source";
-export {TreeState, TreeItemState, AnyTreeState} from "./tree-source/tree-state";
-export {AnyTreeSelection, TreeRange, TreeSelection} from "./tree-source/tree-selection";
+export {TreeState, TreeItemState} from "./tree-source/tree-state";
+export type {AnyTreeState} from "./tree-source/tree-state";
+export {TreeRange, TreeSelection} from "./tree-source/tree-selection";
+export type {AnyTreeSelection} from "./tree-source/tree-selection";
 
 export {ListDataSource} from "./list-source/list-data-source";
-export {GridData, ListData, ListSearchData, ListUniversalData, TableColumn, TableData, ListFlag} from "./list-source/list-data";
-export {ListRange, ListSelection, AnyListSelection} from "./list-source/list-selection";
+export type {GridData, ListData, ListSearchData, ListUniversalData, TableColumn, TableData, ListFlag} from "./list-source/list-data";
+export {ListRange, ListSelection} from "./list-source/list-selection";
+export type {AnyListSelection} from "./list-source/list-selection";
 export {ListState} from './list-source/list-state'
 
 export {FilterService, TreeFolderFilterService, TreeItemFilterService} from "./filtering/filter-service";
-export {FilterAdapter, FilterSaveState, FilterReadState} from "./filtering/filter-adapter";
+export type {FilterAdapter, FilterSaveState, FilterReadState} from "./filtering/filter-adapter";
 export {CreateAction} from "./models/create-action";
 export {RenderDataTypes, SortingTypes} from "./models/render-types";
 export {DataSource, Data} from "./constructor";
