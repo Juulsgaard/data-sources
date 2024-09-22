9bce8925464c6b8a1747cd746de8f7ef136fe92c
Frederik Sørensen <frederikjs1@hotmail.com>
2024-09-22 18:28:24 +0200

    Update signal helpers

--- src/lib/signals.ts
+++ src/lib/signals.ts
@@ -33,13 +33,13 @@ export function debouncedSignal<T>(
  * @param delay - The delay between values
  * @param options - Additional options
  */
-export function throttleSignal<T>(
+export function throttledSignal<T>(
   inputSignal: Signal<T>,
   delay = 500,
   options?: { injector?: Injector, emitLeading?: boolean }
 ): Signal<T> {
 
-  if (options?.injector) assertInInjectionContext(throttleSignal);
+  if (options?.injector) assertInInjectionContext(throttledSignal);
 
   const output = signal(inputSignal());
 
@@ -70,8 +70,67 @@ export function throttleSignal<T>(
 }
 
 /**
+ * Create a signal where values are throttled and debounced
+ * @param inputSignal - The input value to throttle and debounce
+ * @param throttleDelay - The delay between value emissions during continual input
+ * @param debounceDelay - The amount of time the input needs to settle before a value is emitted
+ * @param options - Additional options
+ */
+export function debouncedThrottledSignal<T extends string | undefined>(
+  inputSignal: Signal<T>,
+  throttleDelay = 1000,
+  debounceDelay = 300,
+  options?: { injector?: Injector }
+): Signal<T> {
+
+  if (options?.injector) assertInInjectionContext(debouncedThrottledSignal);
+
+  const output = signal(inputSignal());
+
+  let nextValue: T = inputSignal();
+  let longTimeout: number | undefined = undefined;
+
+  function clearThrottle() {
+    if (!longTimeout) return;
+    clearTimeout(longTimeout);
+    longTimeout = undefined;
+  }
+
+  effect(onDestroy => {
+    const value = inputSignal();
+
+    // Set a shorter debounce timout
+    const shortTimeout = setTimeout(() => {
+      output.set(value);
+      clearThrottle();
+    }, debounceDelay);
+
+    onDestroy(() => clearTimeout(shortTimeout));
+
+
+    // Update the pending value for the longer throttle timeout
+    nextValue = value;
+
+    if (longTimeout) return;
+
+
+    // If no throttle timeout exists, schedule one
+    longTimeout = setTimeout(() => {
+      output.set(nextValue);
+      longTimeout = undefined;
+    }, throttleDelay);
+
+  }, {allowSignalWrites: true, injector: options?.injector});
+
+  const onDestroy = options?.injector?.get(DestroyRef) ?? inject(DestroyRef);
+  onDestroy.onDestroy(() => clearTimeout(longTimeout));
+
+  return output;
+}
+
+/**
  * Create a signal where values are throttled and debounced, but where empty strings skip the delay
- * @param inputSignal - The input value to throttle
+ * @param inputSignal - The input value to throttle and debounce
  * @param throttleDelay - The delay between value emissions during continual input
  * @param debounceDelay - The amount of time the input needs to settle before a value is emitted
  * @param options - Additional options
